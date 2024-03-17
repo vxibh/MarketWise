04dd70eb819dfd56d515a75c22c027eccaad7a21
lakshyaA <lakshyaaggarwal2803@gmail.com>
2024-03-17 20:00:36 +0530

    login routes added

--- backend/routes/authUser.routes.js
+++ backend/routes/authUser.routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { createUser } = require("../controller/registerUser.controller.js");
+const { createUser,loginUser } = require("../controller/user.controller.js");
 const router = express.Router();
 
-
+// registering and logining in user by post method
 router.route("/register").post(createUser);
+router.route("/login").post(loginUser);
 
 module.exports = router;
