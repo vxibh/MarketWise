d0d1273cfb3fe5722b0ba1fc2edb705ab6416c9c
vxibh <vxx6hxv@gmail.com>
2024-04-21 21:44:39 +0530

    webhook removed

--- frontend/pages/api/webhook.js
+++ frontend/pages/api/webhook.js
@@ -3,7 +3,7 @@ const stripe = require('stripe')(process.env.STRIPE_SK);
 import {buffer} from 'micro';
 import {Order} from "@/models/Order";
 
-const endpointSecret = "whsec_634d3142fd2755bd61adaef74ce0504bd2044848c8aac301ffdb56339a0ca78d";
+const endpointSecret = "webhook";
 
 export default async function handler(req,res) {
   await mongooseConnect();
